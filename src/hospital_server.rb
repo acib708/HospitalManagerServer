8e85c9735395d2c67101bbbf1c9c8aa13a98cb66
Alejandro Cardenas <acib708@gmail.com>
2013-11-09 14:36:02 -0600

    Added thrift file

--- src/hospital_server.rb
+++ src/hospital_server.rb
@@ -3,10 +3,10 @@ require 'thrift'
 
 db_manager = DBManager.new
 
-processor = Calculator::Processor.new(sb)
-transport = Thrift::ServerSocket.new(9090)
-transportFactory = Thrift::BufferedTransportFactory.new()
-server = Thrift::SimpleServer.new(processor, transport, transportFactory)
-puts "Starting the Calculator server..."
-server.serve()
-puts "done."
+#processor = Calculator::Processor.new(sb)
+#transport = Thrift::ServerSocket.new(9090)
+#transportFactory = Thrift::BufferedTransportFactory.new()
+#server = Thrift::SimpleServer.new(processor, transport, transportFactory)
+#puts "Starting the Calculator server..."
+#server.serve()
+#puts "done."
