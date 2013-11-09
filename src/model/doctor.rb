d80bd297d8eef2e44991da4c9c886a5b2b8f2f17
Alejandro Cardenas <acib708@gmail.com>
2013-11-09 14:03:55 -0600

    Completed model doctor

---
+++ src/model/doctor.rb
@@ -0,0 +1,16 @@
+class Doctor
+  attr_accessor :clave, :nombre, :direccion, :especialidad, :telefono, :foto
+
+  def to_s
+    "Clave: #{clave}\nNombre: #{nombre}\nDireccion: #{direccion}\nEspecialidad: #{especialidad}\nTelefono: #{telefono}\nFoto: #{foto}"
+  end
+
+  def to_sql
+    "'#{clave}','#{nombre}','#{direccion}','#{especialidad}','#{telefono}','#{foto}'"
+  end
+
+  def to_sql_update
+    "clave= '#{clave}', nombre= '#{nombre}', direccion= '#{direccion}', especialidad= '#{especialidad}', telefono= '#{telefono}', foto= '#{foto}'"
+  end
+
+end
