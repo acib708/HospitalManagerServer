abf93b1c9ad8b39cb6950ade0ff909b2b13ad1f5
marcelonicolasgomezrivera <marcelogomez321@gmail.com>
2013-11-25 22:31:58 -0600

    Re-generated thrift files to add consultarEspecialidades

--- src/testing.rb
+++ src/testing.rb
@@ -3,6 +3,15 @@ require 'thrift'
 require './actions'
 
 db = DBManager.new
-doctor = Doctor.new
-doctor.clave = 'doococd'
-puts "Hello Dante! #{db.consultarEspecialidades}"
+a_doctor = Doctor.new
+a_doctor.clave = 'D100'
+a_doctor.nombre = 'Marcelo Gómez'
+a_doctor.direccion = 'Chichonal'
+a_doctor.especialidad = 'Proctología'
+a_doctor.telefono = '7272727'
+
+db.capturarDoctor a_doctor
+doctores = db.consultarDoctores
+doctores.each do |doctor|
+    puts doctor.nombre
+end
